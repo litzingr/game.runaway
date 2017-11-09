59e92814be984403bb978076a6139e4b0434d26d
Zach Litzinger <z_wolfe@mac.com>
2017-11-09 22:12:44 +0100

    preventing scrolling from arrow keys

--- script.js
+++ script.js
@@ -86,6 +86,13 @@ document.addEventListener("touchmove", function (e) {
     e.preventDefault();
 }, false);
 
+document.addEventListener("keydown", function(e) {
+    // space and arrow keys
+    if([32, 37, 38, 39, 40].indexOf(e.keyCode) > -1) {
+        e.preventDefault();
+    }
+}, false);
+
 function touchMovement() {
     "use strict";
     if (!(touches.x1 === undefined) && !(touches.y1 === undefined) && !(touches.x2 === undefined) && !(touches.y2 === undefined)) {
